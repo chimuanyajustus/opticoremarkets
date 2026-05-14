e84f6d30218ceacb8883b3b00c997a2bafe9182d
chimuanyajustus <chimuanyaatugwu@gmail.com>
2026-05-14 15:36:20 -0700

    mad

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/opticoremarkets/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
